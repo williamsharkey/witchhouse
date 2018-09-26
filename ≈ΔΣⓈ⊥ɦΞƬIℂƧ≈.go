fbb23b56226a7a8770c9e62d1b4119ef4145b5f9
William Sharkey <williamsharkey@gmail.com>
2018-09-26 16:09:16 -0400

    ✝

---
+++ ≈ΔΣⓈ⊥ɦΞƬIℂƧ≈.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// main moon tan center, Los Angeles, 2070
+// deprecated
+func main() {
+	Prayer("welcome ")
+	Prayer("aesthetics are deprecated")
+}
+
+// Prayer is
+// Deprecated
+func Prayer(say string) {
+	fmt.Println(say)
+
+}
